3fd773229faef31045f099032d6ffb8706eb248b
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2007-11-15 19:09:38 +0000

    bigger (readable) font for icon caption

--- viewers/video/VideoThumbnail.py
+++ viewers/video/VideoThumbnail.py
@@ -14,6 +14,6 @@ class VideoThumbnail(Thumbnail):
         Thumbnail.__init__(self, 160, 120)
         self.add_image(os.path.join(_PATH, "film.png"))
         self.add_image(thumb, 13, 4, 134, 112)
-        self.add_rect(0, 104, 160, 16, 0x44, 0x44, 0xff, 0xa0)
-        self.add_text(title, 2, 103, theme.font_tiny, "#ffffff")
+        self.add_rect(0, 98, 160, 22, 0x44, 0x44, 0xff, 0xa0)
+        self.add_text(title, 2, 96, theme.font_tiny, "#ffffff")
         self.add_image(theme.btn_load, 128, 88)
