000af92049f97bbeed3465b896bc35c36ae21f80
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2007-11-18 11:35:59 +0000

    removed hardcoded stuff from logo screen

--- viewers/video/LogoScreen.py
+++ viewers/video/LogoScreen.py
@@ -1,4 +1,5 @@
 from viewers.Thumbnail import Thumbnail
+from mediabox import values
 import theme
 
 import os
@@ -15,13 +16,13 @@ class LogoScreen(Thumbnail):
 
         Thumbnail.__init__(self, _WIDTH, _HEIGHT)
         
-        self.add_text("MediaBox", 0, 0,
+        self.add_text(values.NAME, 0, 0,
                       theme.font_headline, "#ffffff")
         self.add_rect(0, 35, _WIDTH, 1, 0xa0, 0xa0, 0xff)                      
         self.add_text("Entertainment in your Hands", 0, 34,
                       theme.font_tiny, "#e0e0e0")
-        self.add_text("v 0.90 - (c) 2007 Martin Grimme", 0, 340,
-                      theme.font_tiny, "#a0a0a0")    
+        self.add_text("v %s - %s" % (values.VERSION, values.COPYRIGHT),
+                      0, 336, theme.font_tiny, "#a0a0a0")    
         self.add_text("http://mediabox.garage.maemo.org", 142, 220,
                       theme.font_plain, "#a0a0a0")    
 
