d6c8b3497e8bd853ea41de9cc99dad79cf5a881b
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2007-12-06 21:35:22 +0000

    added capability

--- mediabox/caps.py
+++ mediabox/caps.py
@@ -3,8 +3,9 @@
 # capabilities are a bit pattern, so every value must be a power of 2
 NONE             = 0
 PLAYING          = 1 << 0
-POSITIONING      = 1 << 1
-TUNING           = 1 << 2   # same as positioning but looking different
-ZOOMING          = 1 << 3
-RECORDING        = 1 << 4
-BOOKMARKING      = 1 << 5
+SKIPPING         = 1 << 1
+POSITIONING      = 1 << 2
+TUNING           = 1 << 3   # same as positioning but looking different
+ZOOMING          = 1 << 4
+RECORDING        = 1 << 5
+ADDING           = 1 << 6
