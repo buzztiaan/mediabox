960c69258a2959ecc6571e1b585e2b8422536706
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-02-03 20:17:53 +0000

    added some comments

--- mediabox/ViewerState.py
+++ mediabox/ViewerState.py
@@ -1,5 +1,8 @@
 class ViewerState(object):
-
+    """
+    Class for remembering the state of a viewer, so that it can be quickly
+    restored again without the viewer's help.
+    """
     __slots__ = ["item_offset", "selected_item", "collection_visible", "caps",
                  "thumbs_loaded"]
 
