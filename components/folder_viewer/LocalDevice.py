970bf6879d9fae521e519d1b83ab2a677e9073de
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-06-15 20:16:24 +0000

    added user friendly menu

--- components/folder_viewer/LocalDevice.py
+++ components/folder_viewer/LocalDevice.py
@@ -5,6 +5,9 @@ import os
 import commands
 
 
+_ROOT_PATH = "MENU"
+
+
 class LocalDevice(Device):
 
     def __init__(self):
@@ -32,9 +35,38 @@ class LocalDevice(Device):
     def get_icon(self):
     
         return theme.viewer_folders_tablet
+
+
+    def get_root(self):
+    
+        return _ROOT_PATH
+        
+        
+    def __ls_menu(self):
+    
+        items = []
+        for name, path, filetype in \
+          [("Sounds", "/home/user/MyDocs/.sounds", File.DIRECTORY),
+           ("Videos", "/home/user/MyDocs/.videos", File.DIRECTORY),
+           ("Images", "/home/user/MyDocs/.images", File.DIRECTORY),
+           ("Documents", "/home/user/MyDocs/.documents", File.DIRECTORY),
+           ("Games", "/home/user/MyDocs/.games", File.DIRECTORY),
+           ("System", "/", File.DIRECTORY),
+           ("Memory Cards", "MMC", File.DIRECTORY)]:
+            item = File()
+            item.name = name
+            item.path = path
+            item.filetype = filetype
+            items.append(item)
+        #end for
+        
+        return items
         
         
     def ls(self, path):
+    
+        if (path == _ROOT_PATH):
+            return self.__ls_menu()
     
         try:
             files = [ f for f in os.listdir(path)
