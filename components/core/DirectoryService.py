785c791ba957d3a15cb0768df6dd4a0a7c290b2d
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-06-28 12:50:37 +0000

    added experimental DirectoryService

---
+++ components/core/DirectoryService.py
@@ -0,0 +1,70 @@
+from com import Component, events
+
+
+class DirectoryService(Component):
+    """
+    Service component for browsing storage devices.
+    The DirectoryService provides a means for accessing devices by using
+    path strings.
+    """
+    
+    def __init__(self):
+    
+        # table: prefix -> device
+        self.__prefixes = {}
+        # table: ID -> prefix
+        self.__idents = {}
+        
+        Component.__init__(self)
+        
+        
+        
+    def handle_event(self, ev, *args):
+    
+        if (ev == events.CORE_SVC_LIST_PATH):
+            path = args[0]
+            if (path.startswith("/")): path = "file://" + path
+            
+            idx = path.find("://")
+            idx = path.find("/", idx + 3)
+            prefix = path[:idx]
+            path = path[idx:]
+            print "PREFIX", prefix, "PATH", path
+            
+            try:
+                return self.__prefixes[prefix].ls(path)
+            except:
+                return []
+            
+        elif (ev == events.CORE_SVC_GET_FILE):
+            path = args[0]
+            print "GET FILE", path
+            if (path.startswith("/")): path = "file://" + path
+                        
+            idx = path.find("://")
+            idx = path.find("/", idx + 3)
+            prefix = path[:idx]
+            path = path[idx:]
+            print "PREFIX", prefix, "PATH", path
+           
+            try:
+                print self.__prefixes, prefix, path
+                return self.__prefixes[prefix].get_file(path)
+            except:
+                return 0
+
+            
+            
+        elif (ev == events.CORE_EV_DEVICE_ADDED):
+            ident, device = args
+            self.__prefixes[device.get_prefix()] = device
+        
+        elif (ev == events.CORE_EV_DEVICE_REMOVED):
+            ident = args[0]
+            try:
+                prefix = self.__idents[ident]
+                del self.__idents[ident]
+                del self.__prefixes[prefix]
+            except:
+                pass
+                
