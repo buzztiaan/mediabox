fdd3a013ae2f50cabb27eaf602c7c8bbdea06a17
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-07-08 21:42:38 +0000

    upnp event subscription

---
+++ upnp/DeviceDescription.py
@@ -0,0 +1,242 @@
+from SOAPProxy import SOAPProxy
+from GenaSocket import GenaSocket
+from utils import logging
+
+import urlparse
+
+
+_NS_DESCR = "urn:schemas-upnp-org:device-1-0"
+
+
+class DeviceDescription(object):
+    """
+    Class for representing a UPnP device description. The service proxies are
+    being loaded when needed to minimize load and memory consumption in an
+    environment with many UPnP devices where we may not be interested in most
+    of them.
+    """
+    
+    def __init__(self, location, dom):
+        """
+        Creates a new DeviceDescription object from the given DOM tree of a
+        device description.
+        """
+        
+        self.__dom = dom
+        
+        # location of the device description
+        self.__location = location
+        
+        # prefix for accessing relative URLs
+        self.__url_base = ""
+
+        self.__device_type = ""
+        self.__udn = ""
+        self.__friendly_name = ""
+        self.__manufacturer = ""
+        self.__model_name = ""
+        self.__model_number = ""
+        self.__model_description = ""
+        self.__presentation_url = "" 
+        self.__url_base = ""
+        
+        # mapping of service type to (SCPDURL, controlURL, eventSubURL)
+        self.__services = {}
+
+        # mapping of service type to a SOAP proxy
+        self.__service_proxies = {}
+        
+
+        self.__icon_url = ""
+        
+        self.__parse_description(dom)
+        
+        
+        
+    def __parse_description(self, dom):
+    
+        self.__url_base = dom.get_pcdata("{%s}URLBase" % _NS_DESCR).strip()
+        if (not self.__url_base):
+            # some devices make life hard and don't tell about their URL base
+            self.__url_base = self.__location[:self.__location.rfind("/")]
+
+        # read device information
+        device = dom.get_child("{%s}device" % _NS_DESCR)
+        self.__device_type = device.get_pcdata("{%s}deviceType" % _NS_DESCR)
+        self.__udn = device.get_pcdata("{%s}UDN" % _NS_DESCR)
+        self.__friendly_name = device.get_pcdata("{%s}friendlyName" % _NS_DESCR)
+        self.__manufacturer = device.get_pcdata("{%s}manufacturer" % _NS_DESCR)
+        self.__model_name = device.get_pcdata("{%s}modelName" % _NS_DESCR)
+        self.__model_number = device.get_pcdata("{%s}modelNumber" % _NS_DESCR)
+        self.__model_description = device.get_pcdata("{%s}modelDescription" % _NS_DESCR)
+        self.__presentation_url = device.get_pcdata("{%s}presentationURL" % _NS_DESCR)
+
+        # read services
+        svclist = device.get_child("{%s}serviceList" % _NS_DESCR)
+        for service in svclist.get_children():
+            svctype = service.get_pcdata("{%s}serviceType" % _NS_DESCR)
+            scpd_url = urlparse.urljoin(self.__url_base,
+                                  service.get_pcdata("{%s}SCPDURL" % _NS_DESCR))
+            ctrl_url = urlparse.urljoin(self.__url_base,
+                               service.get_pcdata("{%s}controlURL" % _NS_DESCR))
+            event_url = urlparse.urljoin(self.__url_base,
+                              service.get_pcdata("{%s}eventSubURL" % _NS_DESCR))
+            
+            self.__services[svctype] = (scpd_url, ctrl_url, event_url)
+            
+            # we use lazy loading for the SOAP proxy, so we don't create it now
+            
+        #end for
+        
+        # read icon if available
+        icons = device.get_child("{%s}iconList" % _NS_DESCR)
+        if (icons):
+            self.__icon_url = urlparse.urljoin(self.__url_base,
+                                               self.__load_icon(icons))
+
+
+    def __load_icon(self, node):
+        
+        def icon_comparator(icon1, icon2):
+            mimetype1 = icon1.get_pcdata("{%s}mimetype" % _NS_DESCR)
+            width1 = int(icon1.get_pcdata("{%s}width" % _NS_DESCR))
+            height1 = int(icon1.get_pcdata("{%s}height" % _NS_DESCR))
+            area1 = width1 * height1
+
+            mimetype2 = icon2.get_pcdata("{%s}mimetype" % _NS_DESCR)
+            width2 = int(icon2.get_pcdata("{%s}width" % _NS_DESCR))
+            height2 = int(icon2.get_pcdata("{%s}height" % _NS_DESCR))
+            area2 = width2 * height2
+
+            # sort by mimetype and size (PNG is preferred because it
+            # supports transparency)
+            if (mimetype1 == mimetype2):
+                return cmp(area1, area2)
+            else:
+                if (mimetype1 == "image/png"):
+                    return 1
+                else:
+                    return -1
+            #end if        
+
+        icons = node.get_children()
+        icons.sort(icon_comparator)
+        preferred_icon = icons[-1]
+
+        return preferred_icon.get_pcdata("{%s}url" % _NS_DESCR)
+
+
+    def get_url_base(self):
+        """
+        Returns the URL base used by the device.
+        """
+        
+        return self.__url_base
+
+
+    def get_friendly_name(self):
+        """
+        Returns the friendly (i.e. human readable) name of the device.
+        """
+    
+        return self.__friendly_name
+
+
+    def get_icon_url(self, ideal_width, ideal_height):
+        """
+        Returns the URL of an icon which matches the given size best.
+        Returns an empty string if the device has no icons.
+        """
+        
+        # TODO: find best match
+        return self.__icon_url
+
+
+    def get_device_type(self):
+        """
+        Returns the type of device.
+        """
+        
+        return self.__device_type
+        
+        
+    def get_udn(self):
+        """
+        Returns the UDN of the device.
+        """
+        
+        return self.__udn
+        
+        
+    def get_model_description(self):
+        """
+        Returns the human readable model description text.
+        """
+        
+        return self.__model_description
+        
+        
+    def get_presentation_url(self):
+        """
+        Returns the presentation URL of the device. This is typically the
+        URL of a web interface.
+        """
+        
+        return self.__presentation_url
+        
+        
+    def list_services(self):
+        """
+        Returns a list of the supported services.
+        """
+        
+        return self.__services.keys()
+        
+        
+    def get_service_proxy(self, service):
+        """
+        Returns the service proxy object for the given service.
+        Raises a KeyError if the service is not supported.
+        """
+    
+        # lazy loading of the SOAP proxy
+        try:
+            proxy = self.__service_proxies[service]
+        except KeyError:
+            # the following line may raise another KeyError
+            scpd_url, ctrl_url, event_url = self.__services[service]
+            proxy = SOAPProxy(ctrl_url, service, scpd_url)
+            self.__service_proxies[service] = proxy
+            
+        return proxy
+        
+        
+    def subscribe(self, service, cb):
+        """
+        Subscribes to the given service and registers a callback for
+        handling incoming events on this URL.
+        """
+
+        scpd_url, ctrl_url, ev_url = self.__services[service]
+        gena = GenaSocket()
+        gena.subscribe(ev_url, cb)
+        
+        
+    def unsubscribe(self, cb):
+        """
+        Unsubscribes the given callback.
+        """
+        
+        gena = GenaSocket()
+        gena.unsubscribe(cb)
+
+
+
+    def _dump_xml(self):
+        """
+        Dumps the description XML.
+        """
+        
+        logging.debug("Device Description [%s]\n%s" \
+                      % (self.__location, self.__dom._dump()))
+
---
+++ upnp/GenaSocket.py
@@ -0,0 +1,182 @@
+from utils.GAsyncHTTPConnection import GAsyncHTTPConnection, parse_addr
+from utils import logging
+
+import socket
+import commands
+import fcntl
+import struct
+import gobject
+
+
+# TODO: gupnp-network-light crashes when timeout is omitted and MediaBox
+# causes high CPU load -> investigate!
+_GENA_SUBSCRIBE = "SUBSCRIBE %s HTTP/1.1\r\n" \
+                  "HOST: %s\r\n" \
+                  "CALLBACK: <%s>\r\n" \
+                  "NT: upnp:event\r\n" \
+                  "TIMEOUT: Second-infinite\r\n" \
+                  "\r\n"
+
+_GENA_RENEW = "SUBSCRIBE: %s HTTP/1.1\r\n" \
+              "HOST: %s\r\n" \
+              "SID: %s\r\n" \
+              "TIMEOUT: Second-infinite\r\n" \
+              "\r\n"
+                  
+_GENA_UNSUBSCRIBE = "UNSUBSCRIBE %s HTTP/1.1\r\n" \
+                    "HOST: %s\r\n" \
+                    "SID: %s\r\n" \
+                    "\r\n"                  
+
+
+
+def _get_my_ip():
+    """
+    Returns the IP of this host on the network.
+    """
+    
+    # find the network interface (look at the default gateway)
+    iface = commands.getoutput("cat /proc/net/route" \
+                               "|cut -f1,2" \
+                               "|grep 00000000" \
+                               "|cut -f1") \
+            or "lo"
+
+    # get the IP of the interface
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', iface[:15])
+    )[20:24])
+
+
+_PORT = 5555
+
+
+class _GenaSocket(object):
+    """
+    Class for a GENA socket for subscribing/unsubscribing to GENA events
+    and dispatching incoming events to registered callback handlers.
+    """
+
+    def __init__(self):
+    
+        # table: SID -> callback
+        self.__handlers = {}
+        
+        # table: callback -> SID
+        self.__sids = {}
+        
+        # table: SID -> ev_url
+        self.__event_urls = {}
+        
+        
+        # network location of this GENA socket
+        self.__gena_url = ""
+
+        # the socket for receiving GENA events; this is created only when
+        # needed, i.e. once subscriptions are made
+        self.__gena_socket = None
+                
+        
+    def __create_gena_socket(self):
+        """
+        Creates an asynchronous socket for listening to GENA events.
+        """
+        
+        self.__gena_url = "http://%s:%d" % (_get_my_ip(), _PORT)
+        
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("", _PORT))
+        sock.listen(1)
+        
+        gobject.io_add_watch(sock, gobject.IO_IN, self.__on_new_client)
+        
+        return sock
+        
+        
+    def __on_new_client(self, sock, condition):
+    
+        cnx, addr = sock.accept()
+        
+        # lists are passed by reference, so we use a list to hold the data
+        data = [""]
+        gobject.io_add_watch(cnx, gobject.IO_IN, self.__on_receive_data, data)
+        
+        return True
+        
+        
+    def __on_receive_data(self, cnx, condition, data):
+    
+        data[0] += cnx.recv(4096)
+        
+        # TODO: this check for EOF is bad; improve it
+        if (data[0].strip()[-1] == ">"):
+            print "closing socket"
+            cnx.close()
+            self.__process_event(data[0])
+            return False
+            
+        else:
+            return True
+
+
+    def __process_event(self, data):
+    
+        print "received", data
+            
+            
+    def __on_receive_confirmation(self, success, cnx, response, ev_url, cb):
+    
+        if (success):
+            print response.get_status()
+            print response.get_headers()
+            sid = response.get_header("SID")
+            logging.debug("received SID: %s" % sid)
+            self.__handlers[sid] = cb
+            self.__sids[cb] = sid
+            self.__event_urls[sid] = ev_url
+        
+            # TODO: schedule renewal
+        
+        
+    def subscribe(self, ev_url, cb):
+        """
+        Subscribes the given callback to the given event URL.
+        """
+
+        # create GENA socket when first used
+        if (not self.__gena_socket):
+            self.__gena_socket = self.__create_gena_socket()
+
+        host, port, path = parse_addr(ev_url)
+        GAsyncHTTPConnection(host, port,
+                             _GENA_SUBSCRIBE % (path, host, self.__gena_url),
+                             self.__on_receive_confirmation, ev_url, cb)
+        
+
+
+    def unsubscribe(self, cb):
+        """
+        Unsubscribes the given callback.
+        """
+
+        if (cb in self.__sids):
+            sid = self.__sids[cb]
+            ev_url = self.__event_urls[sid]
+            
+            host, port, path = parse_addr(ev_url)
+            GAsyncHTTPConnection(host, port,
+                                _GENA_UNSUBSCRIBE % (path, host, sid),
+                                lambda abc:True)
+            del self.__handlers[sid]
+            del self.__sids[cb]
+            del self.__event_urls[sid]
+        #end if
+
+        
+_singleton = _GenaSocket()
+def GenaSocket(): return _singleton
+
