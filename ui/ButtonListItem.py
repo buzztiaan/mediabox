5adec2b07d6375e2f59d2c447bd3d44c847e7921
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-07-11 22:14:20 +0000

    list stuff

---
+++ ui/ButtonListItem.py
@@ -0,0 +1,92 @@
+from ListItem import ListItem
+from Pixmap import Pixmap, TEMPORARY_PIXMAP
+import theme
+
+
+
+_CLOSED = 0
+_OPEN = 1
+
+_GAP_SIZE = 120
+
+
+class ButtonListItem(ListItem):
+    """
+    Base class for list items with buttons.
+    """
+
+    BUTTON_MENU = "menu"
+
+    # override these by your subclass
+    _ITEMS_OPEN = []
+    _ITEMS_CLOSED = []
+    
+    _BUTTONS = []
+
+
+    def __init__(self):
+
+        self.__state = _CLOSED
+        self.__button_pos = []
+                
+        ListItem.__init__(self)
+
+
+    def open_menu(self):
+    
+        self.__state = _OPEN
+        self.render()
+        
+        
+    def close_menu(self):
+    
+        self.__state = _CLOSED
+        self.render()
+        
+        
+    def get_button_at(self, px):
+    
+        if (self.__state == _CLOSED):
+            idx = 0
+        else:
+            idx = len(self._ITEMS_CLOSED)
+            
+        for pos in self.__button_pos:
+            if (px > pos): return self._BUTTONS[idx]
+            idx += 1
+        #end for
+        
+        return ""
+
+
+    def render_buttons(self, canvas):
+        """
+        Renders the buttons.
+        """
+
+        w, h = canvas.get_size()
+        
+        if (self.__state == _CLOSED):
+            items = self._ITEMS_CLOSED
+        
+        elif (self.__state == _OPEN):
+            items = self._ITEMS_OPEN
+
+        else:
+            items = []
+
+        if (self.__state == _OPEN):
+            menu_width = len(items) * _GAP_SIZE - 16
+            canvas.draw_frame(theme.panel, w - 16 - menu_width, 8,
+                              menu_width, 64, True)
+                              
+        self.__button_pos = []
+        x = w - 8
+        for i in items:
+            canvas.draw_pixbuf(i, 
+                               x - _GAP_SIZE + (_GAP_SIZE - i.get_width()) / 2,
+                               (h - i.get_height()) / 2)
+            self.__button_pos.append(x - _GAP_SIZE)
+            x -= _GAP_SIZE
+        #end for
+
---
+++ ui/ListItem.py
@@ -0,0 +1,69 @@
+from StripItem import StripItem
+import theme
+
+
+class ListItem(StripItem):
+    """
+    Base class for regular hilightable list items.
+    """
+
+    def __init__(self):
+
+        self.__normal_bg = None
+        self.__hilighted_bg = None
+
+        self.__color_1 = "#000000"
+        self.__color_2 = "#666666"
+        self.__font = None
+        self.__grip = None
+        
+        StripItem.__init__(self)
+        
+
+    def set_graphics(self, normal, hilighted):
+    
+        self.__normal_bg = normal
+        self.__hilighted_bg = hilighted
+
+
+    def set_grip(self, pbuf):
+    
+        self.__grip = pbuf
+     
+
+    def set_colors(self, col1, col2):
+    
+        self.__color_1 = col1
+        self.__color_2 = col2
+        
+        
+    def set_font(self, font):
+    
+        self.__font = font  
+
+       
+    def render_label(self, canvas, x, label1, label2 = ""):
+    
+        canvas.draw_text("%s\n<span color='%s'>%s</span>" \
+                          % (label1, self.__color_2, label2),
+                             self.__font, x, 8,
+                             self.__color_1, use_markup = True)        
+       
+        
+    def render_this(self, cnv):
+
+        w, h = self.get_size()    
+        cnv.fill_area(0, 0, w, h, theme.color_bg)        
+
+        background = self.is_hilighted() and self.__hilighted_bg \
+                                         or self.__normal_bg
+
+        if (background):
+            cnv.draw_frame(background, 0, 0, w, h, True)
+
+        x = 0
+        if (self.__grip):
+            x += 4
+            cnv.draw_pixbuf(self.__grip, x, 0)
+            x += 24
+
