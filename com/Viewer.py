74d57e6f4a73834ca78c32d9995922ab6fb5ae36
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-07-26 21:05:57 +0000

    got rid of ToolbarSet class

--- com/Viewer.py
+++ com/Viewer.py
@@ -1,7 +1,6 @@
 from Component import Component
 import msgs
 from ui.Widget import Widget
-from mediabox.ToolbarSet import ToolbarSet
 
 
 class Viewer(Component, Widget):
@@ -15,7 +14,7 @@ class Viewer(Component, Widget):
     
         self.__is_active = False
         
-        self.__current_tbar_set = None
+        self.__current_tbar_set = []
         self.__collection = []
         self.__title = ""
         self.__info = ""
@@ -24,27 +23,14 @@ class Viewer(Component, Widget):
         Widget.__init__(self)
 
 
-    def new_toolbar_set(self, *widgets):
+    def set_toolbar(self, widgets):
         """
-        Creates and returns a new toolbar set.
+        Sets the toolbar from the given widgets.
         """
         
-        tbset = ToolbarSet()
-        for w in widgets:
-            tbset.add_widget(w)
-        
-        self.__current_tbar_set = tbset
-        return tbset
-        
-        
-    def set_toolbar_set(self, tbset):
-        """
-        Sets the given toolbar set on the toolbar.
-        """
-
-        self.__current_tbar_set = tbset
+        self.__current_tbar_set = widgets
         if (self.__is_active):
-            self.emit_event(msgs.CORE_ACT_SET_TOOLBAR, tbset)
+            self.emit_event(msgs.CORE_ACT_SET_TOOLBAR, self.__current_tbar_set)
 
 
     def set_title(self, title):
@@ -73,7 +59,7 @@ class Viewer(Component, Widget):
         self.set_visible(True)        
         self.__is_active = True
         
-        self.set_toolbar_set(self.__current_tbar_set)
+        self.set_toolbar(self.__current_tbar_set)
         self.set_collection(self.__collection)
         self.set_title(self.__title)
         self.set_info(self.__info)
