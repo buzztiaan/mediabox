88384dd79373d763c0005a335d00e911a5fbd3a1
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-11-08 20:34:47 +0000

    command line options for setting log level

--- MediaBox.py
+++ MediaBox.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
+from com import Container
+from utils import logging
+import os
+import sys
+
 # bad hack!
 # work around broken D-Bus bindings on OS 2006; this breaks urllib2 for us,
 # but we don't use it anyway
@@ -7,16 +12,24 @@ def f(*args): raise RuntimeError("Ignore me...")
 import urllib2
 urllib2.AbstractHTTPHandler.do_open = f
 
-
-from utils import logging
-logging.set_level(logging.INFO)
+# TODO: use getopt
+try:
+    arg = sys.argv[1]
+except:
+    arg = ""
+if (arg == "-vvv"):
+    logging.set_level(logging.DEBUG)
+elif (arg == "-vv"):
+    logging.set_level(logging.INFO)
+elif (arg == "-v"):
+    logging.set_level(logging.WARNING)
+elif (arg == "-q"):
+    logging.set_level(logging.OFF)
+else:
+    logging.set_level(logging.ERROR)
 
 
 logging.debug("initializing application")
-
-
-from com import Container
-import os
 container = Container(os.path.join(os.path.dirname(__file__), "components"))
 
 logging.debug("running application")
