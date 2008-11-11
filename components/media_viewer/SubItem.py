51426c893ff67973e3be0ba189aeeadf6b258c55
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-11-11 20:27:52 +0000

    use a different background for subitems

--- components/media_viewer/SubItem.py
+++ components/media_viewer/SubItem.py
@@ -14,7 +14,9 @@ class SubItem(ListItem):
         
     def render_this(self, cnv):
     
-        self.render_bg(cnv)       
+        #self.render_bg(cnv)
+        w, h = self.get_size()
+        cnv.fill_area(0, 0, w, h, "#dddddd")
         self.render_label(cnv, 48, self.__label, self.__sublabel)
         self.render_selection_frame(cnv)
         self.render_buttons(cnv)
