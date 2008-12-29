e586cc8bd20ee977cb37f57641236d240fb3c8a1
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2008-12-29 13:05:56 +0000

    raised version number to 0.96.1

--- mediabox/values.py
+++ mediabox/values.py
@@ -2,7 +2,7 @@ import os
 
 NAME = "MediaBox"
 OSSO_NAME = "de.pycage.mediabox"
-VERSION = "0.96"
+VERSION = "0.96.1"
 
 AUTHORS = ["Martin Grimme  <martin.grimme@lintegra.de>"]
 COPYRIGHT = "\xc2\xa9 2007 - 2008 Martin Grimme"
