6fc7e596b2b7a06d803acf41a509118aacca1ee5
pycage <pycage@578a2022-4d3a-0410-a642-c1cf5df16771>
2010-05-30 17:20:19 +0000

    fixed D-Bus interface issues

--- mediabox/values.py
+++ mediabox/values.py
@@ -2,8 +2,8 @@ import os
 import time
 
 NAME = "MediaBox"
-OSSO_NAME = "de.pycage.mediabox"
-VERSION = "2010.05.29"
+OSSO_NAME = "de.pycage.maemo-mediabox"
+VERSION = "2010.05.30"
 
 AUTHORS = ["Martin Grimme  <martin.grimme@lintegra.de>"]
 COPYRIGHT = "\xc2\xa9 2007 - 2010 Martin Grimme"
